e762361060222afed097114e8adde2b2eefc51cf
Enzo <enzoponf3@gmail.com>
2020-08-03 11:28:59 -0300

    Details and layout modifications

--- scripts.js
+++
@@ -1,7 +0,0 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.0519752, lng: -76.314270999999}, // lat/long of center of map
-    zoom: 15, // 8 or 9 is typical zoom 
-  });
-}
