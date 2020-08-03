5f141bcc580255de7e3dbcdddc60efb632e5e850
Enzo <enzoponf3@gmail.com>
2020-08-03 19:00:04 -0300

    Changed direction of animation

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 var tl = new TimelineMax()
 const controller = new ScrollMagic.Controller()
 
-tl.from('.odd',.5,{x:100, opacity:0})
-tl.from('.even',.5,{x:-100, opacity:0})
+tl.from('.odd',.5,{y:100, opacity:0})
+tl.from('.even',.5,{y:100, opacity:0})
 
 const scene = new ScrollMagic.Scene({
   triggerElement:"#services-title"
