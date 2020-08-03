a014f326114f04a7166f8679b13779a3379d2c79
Enzo <enzoponf3@gmail.com>
2020-08-03 17:00:39 -0300

    New dispotition of services tiles and animation on scroll

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var tl = new TimelineMax()
+const controller = new ScrollMagic.Controller()
+
+tl.from('.odd',.5,{x:100, opacity:0})
+tl.from('.even',.5,{x:-100, opacity:0})
+
+const scene = new ScrollMagic.Scene({
+  triggerElement:"#services-title"
+})
+.setTween(tl)
+  .addTo(controller)
+  .reverse(false)
